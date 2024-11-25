d2baf593355d63f5b29f85b4f26dff6192357e09
Magzhan Dautbekov <magzhandautbekov@gmail.com>
2024-11-25 22:05:39 +0500

    создал новый метод makeSound

--- Greetings.java
+++ Greetings.java
@@ -1,6 +1,11 @@
 public class Greetings {
+    public static void makeSound(){
+        System.out.println("Woof!");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello guys!");
+        makeSound();
     }
 
 }
